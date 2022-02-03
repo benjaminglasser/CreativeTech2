317e551dcdaa3994cacc62a590b1389ea6e6aa40
ben glasser <ben@fantasticvoyage.com>
2022-02-02 19:02:23 -0800

    react issue test

--- project1/week2/css-3/src/components/Body/Body.js
+++ project1/week2/css-3/src/components/Body/Body.js
@@ -1,12 +1,12 @@
 import Left from "../Left/Left"
-import Right from "../Right/Right"
+// import Right from "../Right/Right"
 import React from 'react';
 
 function Body() {
     return (
         <div className="flex">
             <Left />
-            <Right />
+            {/* <Right /> */}
         </div>
     )
 }
